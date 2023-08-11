abdc6b1b37f25b77a8c5176a9ceccbb721464d48
Ndaruga <ndaruga.francis@gmail.com>
2023-08-11 12:23:46 +0300

    send email

---
+++ send_email.php
@@ -0,0 +1,20 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+
+    $to = "ndaruga.francis@gmail.com";
+    $headers = "From: $email" . "\r\n" .
+               "Reply-To: $email" . "\r\n" .
+               "X-Mailer: PHP/" . phpversion();
+
+    if (mail($to, $subject, $message, $headers)) {
+        echo "success";
+    } else {
+        echo "error";
+    }
+
+}
+?>
